c9f49713be7ed59f408a17da8d7fde229e2a9026
Evgeny Bunakov <goodi@bk.ru>
2020-05-26 01:38:58 +0700

    add onesignal

--- app/src/main/java/ru/goodibunakov/amlabvideo/AmlabApplication.kt
+++ app/src/main/java/ru/goodibunakov/amlabvideo/AmlabApplication.kt
@@ -2,6 +2,7 @@ package ru.goodibunakov.amlabvideo
 
 import androidx.multidex.MultiDexApplication
 import com.facebook.stetho.Stetho
+import com.onesignal.OneSignal
 import ru.goodibunakov.amlabvideo.api.ApiService
 import ru.goodibunakov.amlabvideo.data.database.PlaylistsDatabase
 import ru.goodibunakov.amlabvideo.data.repositories.ApiRepositoryImpl
@@ -24,6 +25,12 @@ class AmlabApplication : MultiDexApplication() {
         super.onCreate()
 
         if (BuildConfig.DEBUG) Stetho.initializeWithDefaults(this)
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init()
 
         apiRepository = ApiRepositoryImpl(this, ApiService.create())
         databaseRepository = DatabaseRepositoryImpl(PlaylistsDatabase.getDatabase(this).playlistsDao())
